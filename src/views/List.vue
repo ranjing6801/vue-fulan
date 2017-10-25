ca40b629f532a65594659167b81df3cb4e3dc68e
ranjing <ranjing6801@163.com>
2017-10-25 13:31:27 +0800

    新增列表页面和详情页面

---
+++ src/views/List.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+      <ul>
+          <li v-for="(list,index) in lists">
+              <router-link :to="{ name: 'person', params: { 'personId': index }}">人员 {{index+1}} -- {{list.name}}</router-link>
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      lists:[
+          {name:'张三'},
+          {name:'李四'},
+          {name:'王五'},
+          {name:'刘六'}
+        ]
+    }
+  },
+  created () {
+    
+  },
+  methods:{
+        goback () {
+            window.history.go(-1);
+        }
+  }
+}
+</script>
+
+<style>
+li:hover{
+  cursor: pointer;
+  background: orange;
+}
+li a{
+  color: #000;
+}
+</style>
---
+++ src/views/Person.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <span class="back" @click="goback"><返回</span>
+        <h2>个人详情--{{id+1}}</h2>
+        <p>性别:{{person.sex}}</p>
+        <p>年龄:{{person.age}}</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      id:'',
+      person: {
+        sex: '男',
+        age: '24'
+      }
+    }
+  },
+  created () {
+    this.id = this.$route.params.personId
+  },
+  methods:{
+        goback () {
+            window.history.go(-1);
+        }
+  }
+}
+</script>
+
+<style scope>
+.back{
+  cursor: pointer;
+  color: green;
+}
+</style>
