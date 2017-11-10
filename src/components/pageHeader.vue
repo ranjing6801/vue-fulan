458de8dacb7af41a5906c6a1a1e963f07b3ed7d9
ranjing <ranjing6801@163.com>
2017-11-10 17:29:15 +0800

    修改header

--- src/components/pageHeader.vue
+++ src/components/pageHeader.vue
@@ -2,25 +2,21 @@
   <div class="page-head-wrap">
       <div class="page-head">
           <div class="page-logo">
-              <img src="../assets/logo.png" alt="logo" />
+              <img src="../assets/logo.png" alt="家校美" />
+              <img src="../assets/app.png" alt="App下载" />
           </div>
-          <div class="page-menu">
-              <div class="page-sub01" :class="sub=='1'?'page-cur':'' ">
-                  <router-link to="/check"><span>测试页面1</span></router-link>
-              </div>
-              <div class="page-sub02" :class="sub=='2'?'page-cur':'' ">
-                  <router-link to="/result"><span>测试页面2</span></router-link>
-              </div>
-              <div class="page-sub03" :class="sub=='3'?'page-cur':'' ">
-                  <router-link to="/out"><span>测试页面3</span></router-link>
-              </div>
-              <div class="page-sub04" :class="sub=='4'?'page-cur':'' ">
-                  <router-link to="/detail"><span>测试页面4</span></router-link>
-              </div>
+          <div v-show="!isLogged" class="page-menu">
+              <span @click="handleLogged">登录</span>
+              <span>注册</span>
           </div>
-          <div class="page-user">
-              <span>欢迎您，siri</span>
-              <i>您是 校长 <em @click="clearSession">退出</em></i>
+          <div v-show="isLogged" class="page-user">
+              <div>
+                <span>欢迎您，{{roleName}} 老师!</span>
+                <i @click="clearSession">[退出]</i>
+              </div>
+              <div class="imgbox">
+                <img src="../assets/head.png" alt="icon" />
+              </div>
           </div>
       </div>
   </div>
@@ -30,19 +26,26 @@
 import $ from 'jquery';
 export default {
   name: 'pageHeader',
-  props: ['sub'],
   data () {
     return {
-
+      isLogged : false,
+      roleName : 'siri'
     }
   },
   created () {
-
+    window.localStorage.getItem('islogged') ? this.isLogged = true : this.isLogged = false;
   },
   methods:{
+    handleLogged () {
+      this.isLogged = true;
+      this.$router.push({path: '/myinfo'});
+      window.localStorage.setItem('islogged','true');
+    },
     clearSession () {
-      window.sessionStorage.clear();
+      this.isLogged = false;
       this.$router.push({path: '/'});
+      window.localStorage.clear();
+      window.sessionStorage.clear();
     }
   }
 }
@@ -52,68 +55,67 @@ export default {
 
 .page-head-wrap{
     width: 100%;
-    height: 80px;
-    background: #8cc33c;
+    height: 79px;
+    background: #fff;
+    border-bottom: 1px solid #e6e6e6;
 }
 .page-head{
     width: 1200px;
     height: 80px;
     margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
 }
 .page-logo{
-    float: left;
+    width: 580px;
+    height: 80px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 }
 .page-logo img{
-    display: block;
+  cursor: pointer;
 }
 .page-menu{
-    float: left;
-    margin-left: 120px;
+    width: 112px;
+    height: 80px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 }
-.page-menu a{
-    display: inline-block;
+.page-menu span{
     font-size: 14px;
-    width: 106px;
-    height: 80px;
-    color: #fff;
-}
-.page-sub01,.page-sub02,.page-sub03,.page-sub04{
-    float: left;
-    width: 106px;
-    height: 80px;
-    text-align: center;
-    line-height: 120px;
+    color: #808080;
     cursor: pointer;
-    margin: 0px 10px;
-}
-.page-sub01{
-    background: url(../assets/nav01.png) no-repeat 36px 14px;
-}
-.page-sub02{
-    background: url(../assets/nav02.png) no-repeat 36px 14px;
-}
-.page-sub03{
-    background: url(../assets/nav03.png) no-repeat 36px 14px;
-}
-.page-sub04{
-    background: url(../assets/nav04.png) no-repeat 36px 14px;
-}
-.page-cur{
-    background-color: #578c0a;
 }
 .page-user{
-    float: right;
-    color: white;
-    font-size: 15px;
-    margin-top: 20px;
+    width: 170px;
+    font-size: 14px;
+    color: #808080;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 }
 .page-user span{
     display: block;
     margin-bottom: 4px;
 }
-.page-user em{
+.page-user i{
     cursor: pointer;
     font-size: 12px;
+    display: block;
+    text-align: right;
+}
+.imgbox{
+  width: 40px;
+  height: 40px;
+  overflow: hidden;
+  border-radius: 50%;
+  border: 1px solid transparent;
+}
+.imgbox img{
+  width: 100%;
+  height: 100%;
 }
 
 </style>
