d80ea4d8088db7b7d4f375be1eae38a06d06de69
ranjing <ranjing6801@163.com>
2017-11-14 09:48:54 +0800

    增加创建模态窗口 增加新建试卷页面

---
+++ src/components/createModal.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="create-modal">
+      <div class="content-box">
+          <div class="cont-top">
+              <h3>请选择要创建的内容</h3>
+              <img @click="isClose" src="/static/close.png" alt="x" />
+          </div>
+          <div class="cont-mid">
+              <div @click="warn"><img src="/static/info.png" /><p>通知</p></div>
+              <div class="mg"><img src="/static/work.png" /><p>成绩单</p></div>
+              <div @click="warn"><img src="/static/score.png" /><p>作业</p></div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+
+import $ from 'jquery';
+
+export default {
+  name: 'createModal',
+  data () {
+    return {
+        
+    }
+  },
+  components: {
+      
+  },
+  created () {
+
+  },
+  methods:{
+    isClose () {
+      this.$emit('close','true');
+    },
+    warn () {
+       alert('功能开发中...');
+    }
+  }
+}
+</script>
+
+<style scoped>
+.create-modal{
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  z-index: 2;
+  background: rgba(0, 0, 0, 0.5);
+  position: absolute;
+}
+.content-box{
+  top: 50%;
+  left: 50%;
+  width: 420px;
+  height: 250px;
+  z-index: 3;
+  position: absolute;
+  background: #fff;
+  border-radius: 5px;
+  border: 1px solid #7f7f7f;
+  margin: -125px 0 0 -210px;
+}
+.cont-top{
+  width: 100%;
+  height: 42px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid #dfdfdf;
+}
+.cont-top h3{
+  font-size: 16px;
+  color: #666;
+  margin-left: 20px;
+}
+.cont-top img{
+  margin-right: 20px;
+  cursor: pointer;
+}
+.cont-mid{
+  width: 100%;
+  height: 205px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.cont-mid div{
+  width: 100px;
+  display: flex;
+  flex-wrap: wrap;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+}
+.cont-mid div img{
+  display: block;
+  margin-bottom: 12px;
+}
+.cont-mid div p{
+  font-size: 17px;
+  color: #666;
+}
+.mg{
+  margin: 0 20px;
+}
+</style>
---
+++ src/views/CreatePaper.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="create-paper">
+      <pageheader></pageheader>
+      <navbar></navbar>
+      <div class="content">
+          <div class="cont-left">
+              <div class="cont-left-t">
+                  <div class="cont-step">
+                      <i>1</i>
+                      <span>编辑试卷信息</span>
+                  </div>
+                  <img src="/static/arrow1.png" alt=">" />
+                  <div class="cont-step">
+                      <i>2</i>
+                      <span>录入学生成绩</span>
+                  </div>
+              </div>
+              <div class="cont-left-b">
+                  <div class="title">
+                      <h3>成绩单-2017-11-13</h3>
+                  </div>
+                  <div class="list">
+                      <span>考试时间:</span>
+                      <input type="text" />
+                  </div>
+                  <div class="list">
+                      <span>考试学科:</span>
+                      <input name="subject" type="radio" />语文
+                      <input name="subject" type="radio" />其他
+                  </div>
+                  <div class="list">
+                      <span>考试类型:</span>
+                      <input name="type1" type="radio" />周演练
+                      <input name="type1" type="radio" />期中考试
+                      <input name="type1" type="radio" />期末考试
+                      <input name="type1" type="radio" />月考
+                      <input name="type1" type="radio" />单元测试
+                      <input name="type1" type="radio" />其他
+                  </div>
+                  <div class="list">
+                      <span>考分类型</span>
+                      <input name="type2" type="radio" />等第制
+                      <input name="type2" type="radio" />分数制
+                  </div>
+                  <div v-show="isScore">
+                      <div>
+                        <span>总分</span>
+                        <input type="radio" />150
+                        <input type="radio" />120
+                        <input type="radio" />100
+                        <input type="radio" />自定义
+                      </div>
+                      <div>
+                        <span>合格分</span>
+                        <input type="radio" />90
+                        <input type="radio" />72
+                        <input type="radio" />60
+                        <input type="radio" />自定义
+                      </div>
+                      <div>
+                        <span>优秀分</span>
+                        <input type="radio" />100
+                        <input type="radio" />80
+                        <input type="radio" />60
+                        <input type="radio" />自定义
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="cont-right">
+              <div class="cont-right-t">
+                  <button>下一步</button>
+                  <button>保存</button>
+              </div>
+              <div class="cont-right-b">
+                  <button>发布</button>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+
+import $ from 'jquery';
+
+export default {
+  name: 'createPaper',
+  data () {
+    return {
+        isScore: false
+    }
+  },
+  components: {
+
+  },
+  beforeCreate () {
+    if(!window.localStorage.getItem('islogged')){
+      this.$router.push({path: '/'});
+    }
+  },
+  created () {
+
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style scoped>
+.content{
+  width: 1200px;
+  height: 500px;
+  display: flex;
+  justify-content: space-between;
+}
+.cont-left{
+  width: 880px;
+}
+.cont-left-t{
+
+}
+</style>
